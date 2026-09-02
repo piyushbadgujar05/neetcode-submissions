1707f0866751247398ee871186df859f02851b41
PIYUSH BADGUJAR <160872176+piyushbadgujar05@users.noreply.github.com>
2026-09-02 11:03:22 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        String original=sb.toString();
+        String reversed=sb.reverse().toString();
+        if(original.equals(reversed)){
+            return true;
+        }
+        return false;
+
+    }
+}
